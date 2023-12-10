80bf54c7ae929d5fb3f7ccf768b8865ce6e3b022
JoshHarrisonDev <joshharrison@hotmail.co.uk>
2023-12-10 18:19:50 +0000

    get properties

---
+++ SUPA_BASS/types.d.ts
@@ -0,0 +1,2 @@
+import {Database} from './supabase'
+export type Property = Database["public"]["Tables"]["properties"]["Row"];
--- src/Components/searchbar/Searchbar.tsx
+++ src/Components/searchbar/Searchbar.tsx
@@ -1,34 +1,45 @@
-import {useState} from 'react';
-import './SearchBar.css';
-import {FaSearch} from "react-icons/fa";
-import supabase from "../../../SUPA_BASS/SUPABASS"
+import { useState } from "react";
+import "./SearchBar.css";
+import { FaSearch } from "react-icons/fa";
+import supabase from "../../../SUPA_BASS/SUPABASS";
+import { Property } from "../../../SUPA_BASS/types";
 
-export const Searchbar = () => {
+type SearchBarProps = {
+  setProperties: React.Dispatch<React.SetStateAction<Property[]>>;
+};
+
+export const Searchbar = ({ setProperties }: SearchBarProps) => {
   const [input, setInput] = useState("");
 
   const fetchData = async (value: string) => {
-    let {data} = await supabase.from('properties').select().like("property_name", `${value}%`)
-    if(data){
-      console.log(data);
-      
-    }
+    let { data } = await supabase
+      .from("properties")
+      .select()
+      .like("property_name", `%${value}%`);
 
-  }
+    if (data) {
+      console.log(data);
+      setProperties(data);
+    }
+  };
 
   const handleChange = (value: string) => {
     setInput(value);
-  }
+  };
 
   const onSearch = async () => {
-     await fetchData(input);
-  }
-    return (
-    <div className='input-wrapper'>
-      <input placeholder='Enter a Location...' value={input} onChange={(e) => handleChange(e.target.value)}/>
-      <FaSearch onClick={()=> onSearch()} id='search-icon'/>
+    await fetchData(input);
+  };
+  return (
+    <div className="input-wrapper">
+      <input
+        placeholder="Enter a Location..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+      <FaSearch onClick={() => onSearch()} id="search-icon" />
     </div>
+  );
+};
 
-  )
-}
-
-export default Searchbar
+export default Searchbar;
